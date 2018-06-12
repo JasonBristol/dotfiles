b049889b3a18dd69a7d675848b035d18bacf9d1b
Jason Bristol <json.bristol@gmail.com>
2018-06-11 23:36:11 -0400

    Update casks

--- brew_cask.sh
+++ brew_cask.sh
@@ -1,8 +1,16 @@
 # Apps
 brew cask install transmission
+brew cask install vlc
+
+# Dev
 brew cask install java
 brew cask install iterm2
+brew cask install virtualbox
+brew cask install vagrant
 
 # IDE/Editors
 brew cask install jetbrains-toolbox
 brew cask install visual-studio-code
+
+# Browsers
+brew cask install google-chrome
