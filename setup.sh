3c91980df6a7b3f8eeb0beb3419de6d4c32e22f5
Jason Bristol <json.bristol@gmail.com>
2018-06-11 23:42:25 -0400

    Update powerlevel9k path

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ bash brew_cask.sh
 bash symlink-setup.sh
 
 # Install theme
+rm ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # set up osx defaults
