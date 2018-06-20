a02ef1df454ddf09f51c3080ad2bc9f02d64ead1
Jason Bristol <json.bristol@gmail.com>
2018-06-20 01:52:29 -0400

    Add GPG signing

--- brew.sh
+++ brew.sh
@@ -26,7 +26,7 @@ brew install wget --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 # brew install pinentry-mac
-# brew install gnupg
+brew install gnupg
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
