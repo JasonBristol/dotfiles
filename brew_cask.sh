16e629c6413a01303ab7b9ddb12b0e0d00320ca5
Jason Bristol <json.bristol@gmail.com>
2020-04-13 16:46:55 -0400

    Cleanup casks

--- brew_cask.sh
+++ brew_cask.sh
@@ -1,12 +1,11 @@
 # Apps
-brew cask install transmission
 brew cask install vlc
 brew cask install slack
 brew cask install discord
 brew cask install sonic-pi
 brew cask install dropbox
 brew cask install google-backup-and-sync
-brew cask install deluge
+brew cask install folx
 brew cask install lmms
 
 # Dev
@@ -14,16 +13,11 @@ brew cask install caskroom/versions/java8
 brew cask install iterm2
 brew cask install vagrant
 brew cask install postman
-brew cask install cyberduck
 brew cask install wireshark
 brew cask install docker
 brew cask install dotnet-sdk
 brew cask install mysqlworkbench
 brew cask install julia
-
-# Android dev
-brew cask install android-sdk
-brew cask install android-ndk
 
 # IDE/Editors
 brew cask install jetbrains-toolbox
